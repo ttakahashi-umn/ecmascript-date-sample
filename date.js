41aad3a5b50360e07f56d0bd4e20fcf239a528c1
Haruki Ise <>
2023-12-15 14:22:01 +0900

    一ヶ月後の日付取得

--- date.js
+++ date.js
@@ -1,14 +1,8 @@
 // 今日の日付を取得できるnew Dateを格納
 const today = new Date();
 
-console.log(today)
-
-
 // todayの１ヶ月後を計算する
 today.setMonth(today.getMonth() + 1);
-
-console.log(today)
-
 
 // 計算した日付をYYYYMMDD形式の文字列にする
 const y = today.getFullYear();
@@ -21,7 +15,5 @@ const dd = ("00" + d).slice(-2);
 
 const one_month_later = yyyy + mm + dd;
 
-console.log(one_month_later)
-
-
 // ゴール: 今日から数えて１ヶ月後の日付をYYYYYMMDDで表す文字列を表示したい。
+console.log(one_month_later);
